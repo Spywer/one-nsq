dd319adc01b3d77d0a7ff3c6563bdb75e436c8cf
Spywer <spywer@mail.ru>
2021-09-04 12:04:16 +0300

    Fix bugs

--- src/Read.php
+++ src/Read.php
@@ -20,17 +20,15 @@ class Read
         $this->conn = $conn;
     }
 
-    public function val($n)
+    public function val()
     {
 		// 1048576 - 1МБ
 		
-		$str = $this->conn->recv($n > 4 ? 1048576 : $n, SwooleSocketClient::MSG_PEEK | SwooleSocketClient::MSG_WAITALL);
+		$str = $this->conn->recv(1048576, SwooleSocketClient::MSG_PEEK | SwooleSocketClient::MSG_WAITALL);
 		
 		if ($str === false || !isset($str[0])) {
             throw new Exception('read from fail', Exception::CODE_READ_FAIL);
         }
-		
-		if($n > 4) { $str = substr($str, 4); }
 			
 		//var_dump(addslashes($str));
 		
@@ -50,29 +48,15 @@ class Read
 			// Срабатывает при чтении и добавлении сообщения
 			if(substr($string, 8, 2) == 'OK') {
 				
-				//var_dump(addslashes($string));
-				
-				// Срабатывает при добавлении сообщения
-				if(substr($string, 18, 2)) {
-					return Protocol::HEARTBEAT;
-				}
-				
-				//var_dump(addslashes(substr($string, 8, 2)));
-			
-				// Усли будут проблемы с сообщениями, то использовать регулярку вместо следующей инструкции
-				//$string = preg_replace('#^.+\*#', '', $str);
-				
 				$string = substr($string, 14);
 				$string = substr($string, 0, -11);
 				$string = substr($string, 4);
 				
 				$message = new Data($string);
 				
-				if(empty($message->id)) {
+				if(empty($message->id) || empty($message->attempts)) {
 					return Protocol::HEARTBEAT;
 				}
-				
-				//var_dump($message);
 				
 				return $message;
 			}
@@ -89,10 +73,12 @@ class Read
      */
     public function valFixed()
     {
-        $l    = unpack('N', $this->val(4))[1];
-        $ret  = $this->val($l);
-        $code = unpack('N', substr($ret, 0, 4))[1];
-        $ret  = substr($ret, 4);
+		$ret = $this->val();
+
+        $code = unpack('N', substr($ret, 4, 4))[1];
+        $ret  = substr($ret, 8);
+		
+		//var_dump($code);
 		
         if ($code === Protocol::FRAME_TYPE_RESPONSE) {
             return $this->responseFix($ret);
